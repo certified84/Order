7c7d50a5f9e5b9b227daf380205ed309b267cdd8
Samson Achiaga <40539516+certified84@users.noreply.github.com>
2021-10-08 15:55:22 +0100

    Delete Config.kt

--- app/src/main/java/com/certified/order/util/Config.kt
+++
@@ -1,7 +0,0 @@
-package com.certified.order.util
-
-object Config {
-    const val ADMIN_EMAIL = "sirhumble84@gmail.com"
-    const val ADMIN_PASSWORD = "humblesamson"
-    const val GOOGLE_MAPS_API_KEY = "AIzaSyDoY1IQPVzw7SwbH2dCq_ovY-NbmOFTTl8"
-}

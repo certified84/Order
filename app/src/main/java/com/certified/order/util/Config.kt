5fcf1231668a521020510ddf980739177cd4e1e4
Sammie_kt <sirhumble84@gmail.com>
2021-10-08 16:00:26 +0100

    Added some API KEYS and gitignored Config.kt and Mailer.kt files

--- app/src/main/java/com/certified/order/util/Config.kt
+++ app/src/main/java/com/certified/order/util/Config.kt
@@ -4,4 +4,5 @@ object Config {
     const val ADMIN_EMAIL = "sirhumble84@gmail.com"
     const val ADMIN_PASSWORD = "humblesamson"
     const val GOOGLE_MAPS_API_KEY = "AIzaSyDoY1IQPVzw7SwbH2dCq_ovY-NbmOFTTl8"
+    const val PSTK_PUBLIC_KEY="pk_test_9ad2592883f7646302f65dd0ac3bcc3d50e32f99"
 }
